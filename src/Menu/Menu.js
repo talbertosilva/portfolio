e36b7552d5f5b72a5c93f075f253618785217e45
talbertosilva <100167282+talbertosilva@users.noreply.github.com>
2022-03-26 16:32:47 +0000

    first commit

---
+++ src/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Menu.css';
+
+export const Menu = () => {
+    return (
+        <header>
+            <ul className='menuList'>
+                <li>Work</li>
+                <li>Info</li>
+            </ul>
+        </header>
+    )
+}
